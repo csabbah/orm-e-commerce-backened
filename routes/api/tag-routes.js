6df0ea7beacc6e6ea790b4caa09eb0e1a1c51882
Csabbah <csabbah@ryerson.ca>
2022-05-10 11:58:39 -0400

    You can now query for a single tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,8 +23,25 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ['id', 'tag_name'],
+
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
